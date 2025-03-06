4408b391644178f34db9073dec5ab215a762ea0c
rammsstein <dauzer666@gmail.com>
2025-03-06 16:50:18 +0500

    na rabote

--- src/components/UI/ApartmentCard.vue
+++ src/components/UI/ApartmentCard.vue
@@ -5,7 +5,7 @@
 
       <div class="w-1/2 h-full">
         <img
-          src="/src/assets/Banner10.png"
+          src="/src/assets/Banner12.png"
           alt="photo"
           class="w-full h-full object-cover"
         />
@@ -13,19 +13,18 @@
 
       <div class="w-1/2 p-6 flex flex-col justify-between">
         <div>
-          <h2 class="text-2xl font-bold">Название услуги</h2>
+          <h2 class="text-2xl font-bold">Квартира</h2>
           <p class="text-gray-600 mt-2">
-            Краткое описание услуги. Здесь можно добавить несколько строк текста,
-            чтобы заинтересовать клиента.
+            Квартира оснащена абсолютно всем необходимым для комфортного проживания вдали от дома: → Свежее, качественное, выглаженное постельное белье и полотенца; → Одноразовые тапочки и зубные наборы;  → Большой SMART-TV с выходом в интернет;  → Новый дизайнерский ремонт в современном стиле; → Большая, удобная кровать
           </p>
-          <p class="text-xl font-semibold text-blue-600 mt-4">10 000 ₽</p>
+          <p class="text-xl font-semibold text-teal-600 mt-4">20 000 KZt</p>
         </div>
 
         <div class="flex gap-4">
           <button class="bg-gray-200 hover:bg-gray-300 text-black px-6 py-2 rounded-lg">
             Подробнее
           </button>
-          <button class="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded-lg">
+          <button class="bg-teal-600 hover:bg-teal-700 text-white px-6 py-2 rounded-lg">
             Забронировать
           </button>
         </div>
@@ -36,7 +35,7 @@
 
       <div class="w-1/2 h-full">
         <img
-          src="/src/assets/Banner10.png"
+          src="/src/assets/Banner11.png"
           alt="photo"
           class="w-full h-full object-cover"
         />
@@ -44,19 +43,18 @@
 
       <div class="w-1/2 p-6 flex flex-col justify-between">
         <div>
-          <h2 class="text-2xl font-bold">Название услуги</h2>
+          <h2 class="text-2xl font-bold">Квартира</h2>
           <p class="text-gray-600 mt-2">
-            Краткое описание услуги. Здесь можно добавить несколько строк текста,
-            чтобы заинтересовать клиента.
+            Квартира оснащена абсолютно всем необходимым для комфортного проживания вдали от дома: → Свежее, качественное, выглаженное постельное белье и полотенца; → Одноразовые тапочки и зубные наборы;  → Большой SMART-TV с выходом в интернет;  → Новый дизайнерский ремонт в современном стиле; → Большая, удобная кровать
           </p>
-          <p class="text-xl font-semibold text-blue-600 mt-4">10 000 ₽</p>
+          <p class="text-xl font-semibold text-teal-600 mt-4">20 000 KZt</p>
         </div>
 
         <div class="flex gap-4">
           <button class="bg-gray-200 hover:bg-gray-300 text-black px-6 py-2 rounded-lg">
             Подробнее
           </button>
-          <button class="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded-lg">
+          <button class="bg-teal-600 hover:bg-teal-700 text-white px-6 py-2 rounded-lg">
             Забронировать
           </button>
         </div>
@@ -67,7 +65,7 @@
 
       <div class="w-1/2 h-full">
         <img
-          src="/src/assets/Banner10.png"
+          src="/src/assets/Banner8.png"
           alt="photo"
           class="w-full h-full object-cover"
         />
@@ -75,19 +73,18 @@
 
       <div class="w-1/2 p-6 flex flex-col justify-between">
         <div>
-          <h2 class="text-2xl font-bold">Название услуги</h2>
+          <h2 class="text-2xl font-bold">Квартира</h2>
           <p class="text-gray-600 mt-2">
-            Краткое описание услуги. Здесь можно добавить несколько строк текста,
-            чтобы заинтересовать клиента.
+            Квартира оснащена абсолютно всем необходимым для комфортного проживания вдали от дома: → Свежее, качественное, выглаженное постельное белье и полотенца; → Одноразовые тапочки и зубные наборы;  → Большой SMART-TV с выходом в интернет;  → Новый дизайнерский ремонт в современном стиле; → Большая, удобная кровать
           </p>
-          <p class="text-xl font-semibold text-blue-600 mt-4">10 000 ₽</p>
+          <p class="text-xl font-semibold text-teal-600 mt-4">20 000 KZt</p>
         </div>
 
         <div class="flex gap-4">
           <button class="bg-gray-200 hover:bg-gray-300 text-black px-6 py-2 rounded-lg">
             Подробнее
           </button>
-          <button class="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded-lg">
+          <button class="bg-teal-600 hover:bg-teal-700 text-white px-6 py-2 rounded-lg">
             Забронировать
           </button>
         </div>
