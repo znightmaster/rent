c81ebf7aea528a7561546a75f0ed7d58a45b6a04
rammsstein <dauzer666@gmail.com>
2025-03-07 22:44:27 +0500

    na rabote

--- src/components/UI/ApartmentCard.vue
+++ src/components/UI/ApartmentCard.vue
@@ -1,38 +1,17 @@
 <template>
   <div class="relative flex flex-col items-center gap-5 justify-center w-full px-4">
 
-    <div class="w-full max-w-[1400px] flex border rounded-2xl shadow-lg overflow-hidden flex-col md:flex-row">
-      <div class="w-full md:w-1/2 h-60 md:h-auto flex items-center justify-center">
+    <div class="w-ful max-w-[1400px] h-[400px] flex border rounded-2xl shadow-lg overflow-hidden">
+
+      <div class="w-1/3 h-full">
         <img
           src="/src/assets/Banner12.png"
-          alt="photo"
-          class="w-full h-full object-contain"
-        />
-      </div>
-      <div class="w-full md:w-1/2 p-6 flex flex-col justify-between">
-        <div>
-          <h2 class="text-2xl font-bold">Квартира</h2>
-          <p class="text-gray-600 mt-2">
-            Квартира оснащена абсолютно всем необходимым для комфортного проживания вдали от дома: → Свежее, качественное, выглаженное постельное белье и полотенца; → Одноразовые тапочки и зубные наборы; → Большой SMART-TV с выходом в интернет; → Новый дизайнерский ремонт в современном стиле; → Большая, удобная кровать
-          </p>
-          <p class="text-xl font-semibold text-teal-600 mt-4">20 000 KZt</p>
-        </div>
-        <div class="flex gap-4">
-          <button class="bg-gray-200 hover:bg-gray-300 text-black px-6 py-2 rounded-lg">Подробнее</button>
-          <button class="bg-teal-600 hover:bg-teal-700 text-white px-6 py-2 rounded-lg">Забронировать</button>
-        </div>
-      </div>
-    </div>
-
-    <div class="w-full max-w-[1400px] max-h-[400px] flex border rounded-2xl shadow-lg overflow-hidden flex-col md:flex-row">
-      <div class="w-full md:w-1/2 h-60 md:h-full">
-        <img
-          src="/src/assets/Banner9.png"
           alt="photo"
           class="w-full h-full object-cover"
         />
       </div>
-      <div class="w-full md:w-1/2 p-6 flex flex-col justify-between">
+
+      <div class="w-2/3 p-6 flex flex-col justify-between">
         <div>
           <h2 class="text-2xl font-bold">Квартира</h2>
           <p class="text-gray-600 mt-2">
@@ -40,22 +19,29 @@
           </p>
           <p class="text-xl font-semibold text-teal-600 mt-4">20 000 KZt</p>
         </div>
+
         <div class="flex gap-4">
-          <button class="bg-gray-200 hover:bg-gray-300 text-black px-6 py-2 rounded-lg">Подробнее</button>
-          <button class="bg-teal-600 hover:bg-teal-700 text-white px-6 py-2 rounded-lg">Забронировать</button>
+          <button class="bg-gray-200 hover:bg-gray-300 text-black px-6 py-2 rounded-lg">
+            Подробнее
+          </button>
+          <button class="bg-teal-600 hover:bg-teal-700 text-white px-6 py-2 rounded-lg">
+            Забронировать
+          </button>
         </div>
       </div>
     </div>
 
-    <div class="w-full max-w-[1400px] max-h-[400px] flex border rounded-2xl shadow-lg overflow-hidden flex-col md:flex-row">
-      <div class="w-full md:w-1/2 h-60 md:h-full">
+    <div class="w-ful max-w-[1400px] h-[400px] flex border rounded-2xl shadow-lg overflow-hidden">
+
+      <div class="w-1/3 h-full">
         <img
           src="/src/assets/Banner11.png"
           alt="photo"
           class="w-full h-full object-cover"
         />
       </div>
-      <div class="w-full md:w-1/2 p-6 flex flex-col justify-between">
+
+      <div class="w-2/3 p-6 flex flex-col justify-between">
         <div>
           <h2 class="text-2xl font-bold">Квартира</h2>
           <p class="text-gray-600 mt-2">
@@ -63,9 +49,44 @@
           </p>
           <p class="text-xl font-semibold text-teal-600 mt-4">20 000 KZt</p>
         </div>
+
         <div class="flex gap-4">
-          <button class="bg-gray-200 hover:bg-gray-300 text-black px-6 py-2 rounded-lg">Подробнее</button>
-          <button class="bg-teal-600 hover:bg-teal-700 text-white px-6 py-2 rounded-lg">Забронировать</button>
+          <button class="bg-gray-200 hover:bg-gray-300 text-black px-6 py-2 rounded-lg">
+            Подробнее
+          </button>
+          <button class="bg-teal-600 hover:bg-teal-700 text-white px-6 py-2 rounded-lg">
+            Забронировать
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="w-ful max-w-[1400px] h-[400px] flex border rounded-2xl shadow-lg overflow-hidden">
+
+      <div class="w-1/3 h-full">
+        <img
+          src="/src/assets/Banner8.png"
+          alt="photo"
+          class="w-full h-full object-cover"
+        />
+      </div>
+
+      <div class="w-2/3 p-6 flex flex-col justify-between">
+        <div>
+          <h2 class="text-2xl font-bold">Квартира</h2>
+          <p class="text-gray-600 mt-2">
+            Квартира оснащена абсолютно всем необходимым для комфортного проживания вдали от дома: → Свежее, качественное, выглаженное постельное белье и полотенца; → Одноразовые тапочки и зубные наборы;  → Большой SMART-TV с выходом в интернет;  → Новый дизайнерский ремонт в современном стиле; → Большая, удобная кровать
+          </p>
+          <p class="text-xl font-semibold text-teal-600 mt-4">20 000 KZt</p>
+        </div>
+
+        <div class="flex gap-4">
+          <button class="bg-gray-200 hover:bg-gray-300 text-black px-6 py-2 rounded-lg">
+            Подробнее
+          </button>
+          <button class="bg-teal-600 hover:bg-teal-700 text-white px-6 py-2 rounded-lg">
+            Забронировать
+          </button>
         </div>
       </div>
     </div>
@@ -73,7 +94,10 @@
 </template>
 
 <script setup>
+
 </script>
 
 <style lang="scss" scoped>
+
 </style>
+
