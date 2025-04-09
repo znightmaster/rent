ee25c8298d4a4126b32359484d93938012d8873f
nikolay <dauzer666@gmail.com>
2025-04-09 07:10:49 +0500

    nachalopolozheno

--- src/views/FormPage.vue
+++ src/views/FormPage.vue
@@ -12,21 +12,23 @@
       >
 
       <textarea
-      v-model="review.text"
-      rows="3"
-      placeholder="Что понравилось? что нет?"
-      class="p-2 my-20 mx-20 focus:ring-teal-600 border rounded-4 focus:outline-none focus:ring-2"
+        v-model="review.text"
+        rows="3"
+        placeholder="Что понравилось? что нет?"
+        class="p-2 my-20 mx-20 focus:ring-teal-600 border rounded-4 focus:outline-none focus:ring-2"
       >
       </textarea>
 
       <div class="grid gap-20 p-2 my-20 mx-20">
         <label for=""
-        class=""
+               class=""
         >Фото</label>
         <input
           type="file"
-          class="bg-teal-600 text-white px-6 py-2 h-fit rounded-md hover:bg-teal-700 transition"
+          @change="uploadFile"
+          class="w-fit bg-teal-600 text-white px-6 py-2 h-fit rounded-md hover:bg-teal-700 transition"
         >
+        <img :src="previewPhotoPath" class="w-1/4 mt-2">
       </div>
 
       <div
@@ -37,14 +39,15 @@
         <input
           type="checkbox"
           v-model="review.stars"
-          :value="star"
+          :true-value="star"
+          :false-value="null"
           class=""
           :id="`star${star}`"
         >
         <label
           :for="`star${star}`"
         >
-          {{star}}
+          {{ star }}
         </label>
       </div>
 
@@ -53,6 +56,8 @@
           <input
             type="radio"
             id="adv1"
+            v-model="review.isRecommended"
+            :value="false"
             class="mr-2"
           >
           <label for="adv1">Не советую</label>
@@ -60,7 +65,9 @@
         <div>
           <input
             type="radio"
-            id="adv1"
+            id="adv2"
+            v-model="review.isRecommended"
+            :value="true"
             class="mr-2"
           >
           <label for="adv2">Советую</label>
@@ -73,31 +80,54 @@
         Отправить
       </button>
 
-
-
     </form>
-
 
   </div>
 </template>
 
 <script setup>
-import { reactive, ref } from 'vue';
+import { computed, reactive } from 'vue';
+import axios from 'axios';
 
 const review = reactive({
   author: '',
-  starts: null,
+  stars: null,
   text: '',
-  photos: [],
+  photo: null,
   isRecommended: true,
-})
+});
 
-const stars = [1, 2, 3, 4, 5]
+const stars = [1, 2, 3, 4, 5];
+
+const uploadFile = (e) => {
+  const [file] = e.target.files;
+  review.photo = file;
+};
+
+const previewPhotoPath = computed(() => {
+  if (review.photo) {
+    return URL.createObjectURL(review.photo);
+  }
+  return '#';
+});
 
 const submit = () => {
-  console.log('submit!')
-}
-
+  console.log('submit!');
+  axios.post('/api/review', review, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally(() => {
+      console.log('Конец');
+    });
+};
 
 </script>
 
